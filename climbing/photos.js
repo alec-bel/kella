e0b9d2d1d9c7296fd626c5e5201f4014e26e33bc
Alec Beljanski <alec.beljanski@gmail.com>
2025-01-26 12:04:57 -0700

    change gallery spacing and dimensions

--- climbing/photos.js
+++ climbing/photos.js
@@ -1,14 +1,25 @@
 
 // Array of photos with metadata (src, location, date, size)
 const photoData = [ 
-    { src: "/images/photo-gallery/iceberg.jpeg", location: "Antarctica", date: "December, 2014", width: 3, height: 2 },
-    { src: "/images/photo-gallery/albatross-sunset.jpeg", location: "Antarctica", date: "December, 2014", width: 3, height: 1 },
-    { src: "/images/photo-gallery/desert-tortoise.jpeg", location: "Joshua Tree National Park, CA", date: "April, 2023", width: 2, height: 1 },
-    { src: "/images/photo-gallery/elk-marin-headlands.jpeg", location: "Marin Headlands, CA", date: "May, 2019", width: 1, height: 1 },
-    { src: "/images/photo-gallery/leopard-seal.jpeg", location: "Antarctica", date: "January, 2015", width: 2, height: 1 },
-    { src: "/images/photo-gallery/river-beauty-willowherb.jpeg", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
-    { src: "/images/photo-gallery/trail-ten-falls-waterfall-0.jpeg", location: "Trail of Ten Falls, OR", date: "March, 2021", width: 1, height: 2},
 
+    { src: "/images/photo-gallery/river-beauty-willowherb-glacier-slice-1-1.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
+    { src: "/images/photo-gallery/river-beauty-willowherb-glacier-slice-1-2.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },  
+    { src: "/images/photo-gallery/cascade-canyon-grand-teton-slice-1-1.png", location: "Grand Teton National Park, WY", date: "July, 2023", width: 1, height: 2 },
+    { src: "/images/photo-gallery/river-beauty-willowherb-glacier-slice-2-1.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
+    { src: "/images/photo-gallery/river-beauty-willowherb-glacier-slice-2-2.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
+    { src: "/images/photo-gallery/cascade-canyon-grand-teton-slice-2-1.png", location: "Grand Teton National Park, WY", date: "July, 2023", width: 1, height: 2 },
+    { src: "/images/photo-gallery/giant-leopard-moth.png", location: "Red River Gorge, KY", date: "July, 2017", width: 1, height: 1 },
+    { src: "/images/photo-gallery/mt-hood-wilderness-reflection-slice-1-1.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
+    { src: "/images/photo-gallery/mt-hood-wilderness-reflection-slice-1-2.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
+    { src: "/images/photo-gallery/trail-ten-falls-0-slice-1-1.png", location: "Trail of Ten Falls, OR", date: "March, 2021", width: 1, height: 2},
+    { src: "/images/photo-gallery/mt-hood-wilderness-reflection-slice-2-1.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
+    { src: "/images/photo-gallery/mt-hood-wilderness-reflection-slice-2-2.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
+    { src: "/images/photo-gallery/trail-ten-falls-0-slice-2-1.png", location: "Trail of Ten Falls, OR", date: "March, 2021", width: 1, height: 2},
+    { src: "/images/photo-gallery/desert-tortoise-slice-1-1.png", location: "Joshua Tree National Park, CA", date:  "April, 2023", width: 2, height: 1 },
+    { src: "/images/photo-gallery/desert-tortoise-slice-1-2.png", location: "Joshua Tree National Park, CA", date:  "April, 2023", width: 2, height: 1 },
+    { src: "/images/photo-gallery/antarctica-mountains-slice-1-1.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 },
+    { src: "/images/photo-gallery/antarctica-mountains-slice-1-2.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 },
+    { src: "/images/photo-gallery/antarctica-mountains-slice-1-3.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 }, 
 ];
 
 photoData.forEach((photo, index) => {
