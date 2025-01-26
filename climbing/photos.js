df52b5a45ca130e9dd87e0e420a9138b49b5985b
Alec Beljanski <alec.beljanski@gmail.com>
2025-01-26 13:56:42 -0700

    fix photo splitting

--- climbing/photos.js
+++ climbing/photos.js
@@ -1,34 +1,34 @@
 
 // Array of photos with metadata (src, location, date, size)
 // const photoData = [ 
-//     { src: "../images/photo-gallery/river-beauty-willowherb-glacier-slice-1-1.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
-//     { src: "../images/photo-gallery/river-beauty-willowherb-glacier-slice-1-2.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },  
-//     { src: "../images/photo-gallery/cascade-canyon-grand-teton-slice-1-1.png", location: "Grand Teton National Park, WY", date: "July, 2023", width: 1, height: 2 },
-//     { src: "../images/photo-gallery/river-beauty-willowherb-glacier-slice-2-1.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
-//     { src: "../images/photo-gallery/river-beauty-willowherb-glacier-slice-2-2.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
-//     { src: "../images/photo-gallery/cascade-canyon-grand-teton-slice-2-1.png", location: "Grand Teton National Park, WY", date: "July, 2023", width: 1, height: 2 },
-//     { src: "../images/photo-gallery/giant-leopard-moth.png", location: "Red River Gorge, KY", date: "July, 2017", width: 1, height: 1 },
-//     { src: "../images/photo-gallery/mt-hood-wilderness-reflection-slice-1-1.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
-//     { src: "../images/photo-gallery/mt-hood-wilderness-reflection-slice-1-2.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
-//     { src: "../images/photo-gallery/trail-ten-falls-0-slice-1-1.png", location: "Trail of Ten Falls, OR", date: "March, 2021", width: 1, height: 2},
-//     { src: "../images/photo-gallery/mt-hood-wilderness-reflection-slice-2-1.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
-//     { src: "../images/photo-gallery/mt-hood-wilderness-reflection-slice-2-2.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
-//     { src: "../images/photo-gallery/trail-ten-falls-0-slice-2-1.png", location: "Trail of Ten Falls, OR", date: "March, 2021", width: 1, height: 2},
-//     { src: "../images/photo-gallery/desert-tortoise-slice-1-1.png", location: "Joshua Tree National Park, CA", date:  "April, 2023", width: 2, height: 1 },
-//     { src: "../images/photo-gallery/desert-tortoise-slice-1-2.png", location: "Joshua Tree National Park, CA", date:  "April, 2023", width: 2, height: 1 },
-//     { src: "../images/photo-gallery/antarctica-mountains-slice-1-1.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 },
-//     { src: "../images/photo-gallery/antarctica-mountains-slice-1-2.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 },
-//     { src: "../images/photo-gallery/antarctica-mountains-slice-1-3.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 }, 
+//     { src: "/images/photo-gallery/river-beauty-willowherb-glacier-slice-1-1.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
+//     { src: "/images/photo-gallery/river-beauty-willowherb-glacier-slice-1-2.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },  
+//     { src: "/images/photo-gallery/cascade-canyon-grand-teton-slice-1-1.png", location: "Grand Teton National Park, WY", date: "July, 2023", width: 1, height: 2 },
+//     { src: "/images/photo-gallery/river-beauty-willowherb-glacier-slice-2-1.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
+//     { src: "/images/photo-gallery/river-beauty-willowherb-glacier-slice-2-2.png", location: "Russel Glacier, Greenland", date: "June, 2019", width: 2, height: 2 },
+//     { src: "/images/photo-gallery/cascade-canyon-grand-teton-slice-2-1.png", location: "Grand Teton National Park, WY", date: "July, 2023", width: 1, height: 2 },
+//     { src: "/images/photo-gallery/giant-leopard-moth.png", location: "Red River Gorge, KY", date: "July, 2017", width: 1, height: 1 },
+//     { src: "/images/photo-gallery/mt-hood-wilderness-reflection-slice-1-1.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
+//     { src: "/images/photo-gallery/mt-hood-wilderness-reflection-slice-1-2.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
+//     { src: "/images/photo-gallery/trail-ten-falls-0-slice-1-1.png", location: "Trail of Ten Falls, OR", date: "March, 2021", width: 1, height: 2},
+//     { src: "/images/photo-gallery/mt-hood-wilderness-reflection-slice-2-1.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
+//     { src: "/images/photo-gallery/mt-hood-wilderness-reflection-slice-2-2.png", location: "Mt Hood Wilderness", date: "July, 2019", width: 2, height: 2 },
+//     { src: "/images/photo-gallery/trail-ten-falls-0-slice-2-1.png", location: "Trail of Ten Falls, OR", date: "March, 2021", width: 1, height: 2},
+//     { src: "/images/photo-gallery/desert-tortoise-slice-1-1.png", location: "Joshua Tree National Park, CA", date:  "April, 2023", width: 2, height: 1 },
+//     { src: "/images/photo-gallery/desert-tortoise-slice-1-2.png", location: "Joshua Tree National Park, CA", date:  "April, 2023", width: 2, height: 1 },
+//     { src: "/images/photo-gallery/antarctica-mountains-slice-1-1.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 },
+//     { src: "/images/photo-gallery/antarctica-mountains-slice-1-2.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 },
+//     { src: "/images/photo-gallery/antarctica-mountains-slice-1-3.png", location: "Antarctica", date: "January 2015", width: 3, height: 1 }, 
 // ];
 
 // const fullPhotoData = [
-//     { src: "../images/photo-gallery/giant-leopard-moth.png", location: "Red River Gorge, KY", date: "July, 2017"},
-//     { src: "../images/photo-gallery/river-beauty-willowherb-glacier.png", location: "Russel Glacier, Greenland", date: "June, 2019"},
-//     { src: "../images/photo-gallery/cascade-canyon-grand-teton.png", location: "Grand Teton National Park, WY", date: "July, 2023"},
-//     { src: "../images/photo-gallery/mt-hood-wilderness-reflection.png", location: "Mt Hood Wilderness", date: "July, 2019" },
-//     { src: "../images/photo-gallery/trail-ten-falls-0.png", location: "Trail of Ten Falls, OR", date: "March, 2021"},
-//     { src: "../images/photo-gallery/desert-tortoise.png", location: "Joshua Tree National Park, CA", date:  "April, 2023" },
-//     { src: "../images/photo-gallery/antarctica-mountains.png", location: "Antarctica", date: "January 2015" },
+//     { src: "/images/photo-gallery/giant-leopard-moth.png", location: "Red River Gorge, KY", date: "July, 2017"},
+//     { src: "/images/photo-gallery/river-beauty-willowherb-glacier.png", location: "Russel Glacier, Greenland", date: "June, 2019"},
+//     { src: "/images/photo-gallery/cascade-canyon-grand-teton.png", location: "Grand Teton National Park, WY", date: "July, 2023"},
+//     { src: "/images/photo-gallery/mt-hood-wilderness-reflection.png", location: "Mt Hood Wilderness", date: "July, 2019" },
+//     { src: "/images/photo-gallery/trail-ten-falls-0.png", location: "Trail of Ten Falls, OR", date: "March, 2021"},
+//     { src: "/images/photo-gallery/desert-tortoise.png", location: "Joshua Tree National Park, CA", date:  "April, 2023" },
+//     { src: "/images/photo-gallery/antarctica-mountains.png", location: "Antarctica", date: "January 2015" },
 // ]
 
 const photoData = [ 
@@ -71,7 +71,7 @@ fullPhotoData.forEach((photo) => {
 
 photoData.forEach((photo, index) => {
   if (photo.src.includes('-slice')) {
-    const photoName = photo.src.split('-slice')[0] + photo.src.split('.')[1];
+    const photoName = photo.src.split('-slice')[0] + "." + photo.src.split('.')[1];
     photo.fullSrc = fullPhotos[photoName].src;
   } else {
     photo.fullSrc = photo.src;
